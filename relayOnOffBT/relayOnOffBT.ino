71ea4f6f0c0e6145392b97f5edbe11c5db1dfd21
Meir Michanie <meirm@cyborg.fi>
2022-11-29 14:40:08 +0100

    move datecalc to its own repository

--- relayOnOffBT/relayOnOffBT.ino
+++ relayOnOffBT/relayOnOffBT.ino
@@ -6,7 +6,7 @@
 
 #include "BluetoothSerial.h"
 #include "composter.h"
-#include "datecalc.h"
+#include <datecalc.h>
 #include "extras.h"
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
